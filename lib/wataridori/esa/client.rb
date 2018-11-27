d22862b254798fbc7a2a7439c2a1af7d06faf141
kokuyouwind <kokuyouwind@gmail.com>
2018-11-27 14:10:53 +0900

    Wataridori::Esa::ClientでRatelimitを考慮するよう機能追加

--- lib/wataridori/esa/client.rb
+++ lib/wataridori/esa/client.rb
@@ -7,16 +7,24 @@ module Wataridori
     class Client
       def initialize(access_token:, current_team:)
         @original = ::Esa::Client.new(access_token: access_token, current_team: current_team)
+        @ratelimit = Ratelimit.no_wait
       end
 
       private
 
-      attr_reader :original
+      attr_reader :original, :ratelimit
 
       def method_missing(method_name, *args)
-        return original.send(method_name, *args) if original.respond_to?(method_name)
+        return call_original(method_name, *args) if original.respond_to?(method_name)
 
         super
+      end
+
+      def call_original(method_name, *args)
+        sleep ratelimit.second_for_next_request
+        original.send(method_name, *args).tap do |response|
+          @ratelimit = Ratelimit.from_headers(response.headers)
+        end
       end
 
       def respond_to_missing?(method_name, *)
--- spec/wataridori/esa/client_spec.rb
+++ spec/wataridori/esa/client_spec.rb
@@ -3,7 +3,13 @@
 require 'esa'
 
 RSpec.describe Wataridori::Esa::Client do
+  let(:ratelimit_remaining) { '1' }
+  let(:ratelimit_reset_at) { '0' }
   let(:client) { ::Esa::Client.new(access_token: 'dummy_token', current_team: 'dummy_team') }
+  let(:response) do
+    Struct.new(:headers, :body)
+          .new({ 'X-Ratelimit-Remaining' => ratelimit_remaining, 'X-Ratelimit-Reset' => ratelimit_reset_at }, 'dummy_response')
+  end
 
   subject do
     allow(::Esa::Client).to receive(:new).and_return(client)
@@ -13,14 +19,55 @@ RSpec.describe Wataridori::Esa::Client do
   describe 'method_missing' do
     context 'オリジナルのEsa::Clientに存在するメソッド' do
       it 'オリジナルを呼び出す' do
-        expect(client).to receive(:posts).and_return('dummy_posts')
-        expect(subject.posts).to eq('dummy_posts')
+        expect(client).to receive(:posts).and_return(response)
+        expect(subject.posts.body).to eq('dummy_response')
       end
     end
 
     context 'オリジナルのEsa::Clientに存在しないメソッド' do
       it 'NoMethodErrorになる' do
         expect { subject.unexist_method }.to raise_error(NoMethodError)
+      end
+    end
+  end
+
+  describe 'Ratelimit' do
+    before do
+      expect(client).to receive(:posts).and_return(response)
+      subject.posts
+    end
+
+    context '前回リクエストでのRemainingが0かつreset_atが15分後' do
+      let(:ratelimit_remaining) { '0' }
+      let(:ratelimit_reset_at) { Time.now + 15 * 60 }
+
+      it '15分程度sleepする' do
+        # 実行時に数秒の誤差が入りうるので、14分以上のsleepであることを確認する
+        expect(subject).to receive(:sleep).with(be >= 14 * 60).and_return(0)
+        expect(client).to receive(:post).and_return(response)
+        subject.post
+      end
+    end
+
+    context '前回リクエストでのRemainingが0だがreset_atは1秒前' do
+      let(:ratelimit_remaining) { '0' }
+      let(:ratelimit_reset_at) { Time.now - 1 }
+
+      it 'sleepしない' do
+        expect(subject).to receive(:sleep).with(0).and_return(0)
+        expect(client).to receive(:post).and_return(response)
+        subject.post
+      end
+    end
+
+    context '前回リクエストでのRemainingが1でreset_atは15分後' do
+      let(:ratelimit_remaining) { '1' }
+      let(:ratelimit_reset_at) { Time.now + 15 * 60 }
+
+      it 'sleepしない' do
+        expect(subject).to receive(:sleep).with(0).and_return(0)
+        expect(client).to receive(:post).and_return(response)
+        subject.post
       end
     end
   end
