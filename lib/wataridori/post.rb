ba35f2b51e87ca35008e732ca55a323bda6331f3
kokuyouwind <kokuyouwind@gmail.com>
2018-12-05 16:12:18 +0900

    add Wataridori::Post and implement #replace_links

---
+++ lib/wataridori/post.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+
+module Wataridori
+  class Post
+    def initialize(post)
+      @post = post
+    end
+
+    def replace_links(rule)
+      post.body_md = links.inject(post.body_md) do |body_md, link|
+        next body_md unless rule.target?(link)
+
+        if rule.post_relative_link?(link)
+          body_md.gsub(/\(\w*#{link}\w*\)/, "(#{rule.replaced(link)})")
+        else
+          body_md.gsub(link, rule.replaced(link))
+        end
+      end
+
+      self
+    end
+
+    def to_h
+      post
+    end
+
+    private
+
+    attr_reader :post
+
+    def links
+      Nokogiri::HTML.parse(post.body_html).css('a').map { |link| link['href'] }
+    end
+  end
+end
